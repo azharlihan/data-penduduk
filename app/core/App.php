752eab67dd11893b0d193a193e3a0495e6836836
azharlihan <46442361+azharlihan@users.noreply.github.com>
2020-09-19 07:46:59 +0700

    Add routing rules

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,50 @@
 
 class App
 {
+	// Declare properties and intialize default value for each variable
+	private $controller = 'Home';
+	private $method = 'index';
+	private $params = [];
+
+
 	public function __construct()
 	{
-		echo "core/App";
+		$url = $this->parseURL();
+
+		// Set controller name based URL
+		if (file_exists('controllers/' . $url[0] . '.php')) {
+			$this->controller = ucfirst($url[0]);
+			unset($url[0]);
+		}
+
+		// Initialize  controller
+		$this->controller = new $this->controller;
+
+		// Set method name based URL
+		if (isset($url[1])) {
+			if (method_exists($this->controller, $url[1])) {
+				$this->method = strtolower($url[1]);
+				unset($url[1]);
+			}
+		}
+
+		// Set params
+		if (!empty($url)) {
+			$this->params = array_values($url);
+		}
+
+		// Run the method
+		call_user_func_array([$this->controller, $this->method], $this->params);
+	}
+
+	// Parsing to get pretty URL
+	public function parseURL()
+	{
+		if (isset($_GET['url'])) {
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
 	}
 }
--- app/init.php
+++ app/init.php
@@ -9,12 +9,19 @@ require_once 'core/Config.php';
 // Load the real app class
 require_once 'core/App.php';
 
-// Instantiation app
-$app = new App;
-
 // Autoload another core class
 spl_autoload_register(function ($className) {
 	if (file_exists("core/$className.php")) {
 		require_once "core/$className.php";
 	}
 });
+
+// Autoload controller class
+spl_autoload_register(function ($className) {
+	if (file_exists("controllers/$className.php")) {
+		require_once "controllers/$className.php";
+	}
+});
+
+// Instantiation app
+$app = new App;
