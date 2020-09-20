46664f38de28878f32d70745d57c92d3ad83113d
azharlihan <46442361+azharlihan@users.noreply.github.com>
2020-09-20 14:49:03 +0700

    Add method response & postData in Controller Class

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -15,4 +15,24 @@ class Controller
 		$modelName = "\Models\\$model";
 		return new $modelName;
 	}
+
+	public function response($response)
+	{
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
+
+	public function postData($key = null)
+	{
+		if (is_null($key)) {
+			$postData =  filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING, true);
+
+			foreach ($postData as $k => $v) {
+				$postData[$k] = htmlentities(strip_tags(trim($postData[$k])));
+			}
+			return $postData;
+		} else {
+			return htmlentities(strip_tags(trim(filter_input(INPUT_POST, $key, FILTER_SANITIZE_STRING))));
+		}
+	}
 }
